3dc56fe90d13aa6790340bff3d0f3e3c8e00b38a
Francesc Edo Trias <Francesc Edo Trias>
2017-01-08 06:18:00 +0100

    categories beta 2

--- new/js/categories.js
+++ new/js/categories.js
@@ -1,15 +1,41 @@
 $( document ).ready(function() {
 	getSelect();
+	$(document).on("click", '.cat-sub', function(event) { 
+		$(this).toggleClass('selected');
+	});
+	var sd = [];
+	$(document).on("click", '.subsub', function(event) { 
+		sd = [];
+		$(this).parent().parent().children('.subcont').find('.selected').each(function() {
+			var text = $(this).children('.cat-sub-text').text();
+			var id = $(this).children('.cat-sub-id').text();
+			sd.push([id, text]);
+		});
+		// console.log(sd);
+		$('.subsel').fadeOut();
+		jQuery.each( sd, function( i, val ) {
+			$('.cat-content').append(
+				'<div class="sub">'+
+					'<p hidden>'+val[0]+'</p>'+
+					'<p>'+val[1]+'</p>'+
+					'<input type="number">'+
+				'</div>');
+		});
+	});
+	$(document).on("click", '.mylink', function(event) { 
+	    alert("new link clicked!");
+	});
 	$('.add_cat').on( "click", function() {
         $('.select').toggle();
     });
 });
-
+function select (id){
+	console.log('exec');
+}
 function deletee(id) {
 	console.log('exec');
 	$(".cat-selected" ).find(".cat-"+id).parent().remove();
 }
-
 function addcat(id, name){
 	$('.select').hide();
 	getSub(id);
@@ -18,9 +44,17 @@ function addcat(id, name){
 		'<div class="cat-selected">'+
 			'<p hidden class="cat-'+id+'"></p>'+
 			'<div class="cat-header">'+
-				'<p>'+name+'</p><div class="cat-close" onclick="deletee('+id+');"></div>'+
+				'<p>'+name+'</p>'+
+				'<div class="cat-close" onclick="deletee('+id+');"></div>'+
 			'</div>'+
-			'<div class="cat-content"></div>'+
+			'<div class="cat-content">'+
+				'<div class="subsel">'+
+					'<div class="subcont"></div>'+
+					'<div class="subfoot">'+
+						'<button class="subsub">Done</button>'+
+					'</div>'+
+				'</div>'+
+			'</div>'+
 		'</div>');
 }
 
@@ -57,6 +91,7 @@ function getSub(id){
 }
 function listSub(id, array){
 	jQuery.each( array, function( i, val ) {	
-		$( ".cat-selected" ).find(".cat-"+id).parent().children('.cat-content').append('<div class="cat-sub">'+val.name+'<input type="number" name="'+val.id+'" min="1"></div>');
+		$( ".cat-selected" ).find(".cat-"+id).parent().children('.cat-content').children('.subsel').children('.subcont').append(
+				'<div class="cat-sub"><p class="cat-sub-text">'+val.name+'</p><p class="cat-sub-id" hidden>'+val.id+'</p></div>');
 	});
 }
